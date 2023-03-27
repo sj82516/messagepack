6cd45a8f424a0e32e5fd5cab34848d3233920714
yuanchieh <yuanchieh-at@Yuanchieh-3.local>
2023-03-28 01:54:56 +0800

    refactor: chain of responsibility pattern

--- encode.go
+++ encode.go
@@ -1,60 +1,98 @@
 package msgpack
 
-import "reflect"
+import (
+    "errors"
+    "reflect"
+)
+
+type encoder func(interface{}) ([]byte, error)
 
 // Encode encodes the given value into messagepack format byte arrays.
 // The value must be a struct, map, slice, or array.
 func Encode(v interface{}) ([]byte, error) {
-    if v == nil {
-        return encodeNil()
+    encoderCh := []encoder{
+        encodeNil,
+        encodeBool,
+        encodeInt,
+        encodeUint,
     }
     
-    if reflect.TypeOf(v) == reflect.TypeOf(true) {
-        return encodeBool(v.(bool))
-    }
-    
-    if reflect.TypeOf(v) == reflect.TypeOf(int(0)) {
-        return encodeInt(v.(int))
+    for _, encoder := range encoderCh {
+        if b, err := encoder(v); err != nil {
+            return nil, err
+        } else if b != nil {
+            return b, nil
+        }
     }
     
     return nil, nil
 }
 
 // nil
-func encodeNil() ([]byte, error) {
+func encodeNil(v interface{}) ([]byte, error) {
+    if v != nil {
+        return nil, nil
+    }
+    
     return []byte{0xc0}, nil
 }
 
 // bool
-func encodeBool(v bool) ([]byte, error) {
-    if v {
-        return []byte{0xc3}, nil
+func encodeBool(v interface{}) ([]byte, error) {
+    if t, ok := v.(bool); !ok {
+        return nil, nil
+    } else {
+        if t {
+            return []byte{0xc3}, nil
+        }
+        
+        return []byte{0xc2}, nil
     }
-    
-    return []byte{0xc2}, nil
-}
-
-func encodeInt(v int) ([]byte, error) {
-    if v >= 0 {
-        return encodeUint(uint(v))
-    }
-    return nil, nil
-    //return encodeSignInt(v)
 }
 
 // https://github.com/msgpack/msgpack/blob/master/spec.md#int-format-family
-func encodeUint(v uint) ([]byte, error) {
-    if v <= 127 {
-        return []byte{byte(v)}, nil
+func encodeInt(v interface{}) ([]byte, error) {
+    if reflect.TypeOf(v).Kind() != reflect.Int &&
+        reflect.TypeOf(v).Kind() != reflect.Int8 &&
+        reflect.TypeOf(v).Kind() != reflect.Int16 &&
+        reflect.TypeOf(v).Kind() != reflect.Int32 &&
+        reflect.TypeOf(v).Kind() != reflect.Int64 {
+        return nil, nil
     }
-    if v <= 255 {
-        return []byte{0xcc, byte(v)}, nil
+    
+    i := v.(int)
+    if i >= 0 {
+        uI := uint(i)
+        return encodeUint(uI)
     }
-    if v <= 65535 {
-        return []byte{0xcd, byte(v >> 8), byte(v)}, nil
+    return nil, nil
+}
+
+func encodeUint(v interface{}) ([]byte, error) {
+    if reflect.TypeOf(v).Kind() != reflect.Uint &&
+        reflect.TypeOf(v).Kind() != reflect.Uint8 &&
+        reflect.TypeOf(v).Kind() != reflect.Uint16 &&
+        reflect.TypeOf(v).Kind() != reflect.Uint32 &&
+        reflect.TypeOf(v).Kind() != reflect.Uint64 {
+        return nil, nil
     }
-    if v <= 4294967295 {
-        return []byte{0xce, byte(v >> 24), byte(v >> 16), byte(v >> 8), byte(v)}, nil
+    
+    uInt, _ := v.(uint)
+    if uInt < 128 {
+        return []byte{byte(uInt)}, nil
     }
-    return []byte{0xcf, byte(v >> 56), byte(v >> 48), byte(v >> 40), byte(v >> 32), byte(v >> 24), byte(v >> 16), byte(v >> 8), byte(v)}, nil
+    if uInt < 256 {
+        return []byte{0xcc, byte(uInt)}, nil
+    }
+    if uInt < 65536 {
+        return []byte{0xcd, byte(uInt >> 8), byte(uInt)}, nil
+    }
+    if uInt < 4294967296 {
+        return []byte{0xce, byte(uInt >> 24), byte(uInt >> 16), byte(uInt >> 8), byte(uInt)}, nil
+    }
+    if uInt < 18446744073709551615 {
+        return []byte{0xcf, byte(uInt >> 56), byte(uInt >> 48), byte(uInt >> 40), byte(uInt >> 32), byte(uInt >> 24), byte(uInt >> 16), byte(uInt >> 8), byte(uInt)}, nil
+    }
+    
+    return nil, errors.New("out of range")
 }
